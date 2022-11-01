c080ab43caffff988e7cddd414368b82e387d7d7
Martijn Bulinga <94762243+skorpioo@users.noreply.github.com>
2022-11-01 13:27:04 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,40 +1,46 @@
-const menuBtn = document.querySelector(".menu-button");
-const sideNav = document.querySelector("nav");
-const closeBtn = document.querySelector(".close-button");
-const bodyOverlay = document.querySelector(".menu-overlay");
-const dropdownLink = document.querySelector("[data-dropdown-link]");
-const dropdownMenu = document.querySelector("[data-dropdown-menu]");
+const menuBtn = document.querySelector('.menu-button')
+const sideNav = document.querySelector('nav')
+const closeBtn = document.querySelector('.close-button')
+const bodyOverlay = document.querySelector('.menu-overlay')
+const dropdownLink = document.querySelector('[data-dropdown-link]')
+const dropdownMenu = document.querySelector('[data-dropdown-menu]')
+const rotateArrowA = document.querySelector('.arrow-a')
+const rotateArrowB = document.querySelector('.arrow-b')
 
-menuBtn.addEventListener("click", function () {
-	sideNav.classList.add("open");
-	bodyOverlay.classList.add("enabled");
-});
+menuBtn.addEventListener('click', function () {
+	sideNav.classList.add('open')
+	bodyOverlay.classList.add('enabled')
+})
 
-closeBtn.addEventListener("click", function () {
-	sideNav.classList.remove("open");
-	bodyOverlay.classList.remove("enabled");
-});
+closeBtn.addEventListener('click', function () {
+	sideNav.classList.remove('open')
+	bodyOverlay.classList.remove('enabled')
+})
 
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
 function dropDown1() {
-	document.getElementById("dropdown-1").classList.toggle("show");
+	document.getElementById('dropdown-1').classList.toggle('show')
+	document.getElementById('dropdown-2').classList.remove('show')
+	rotateArrowA.classList.toggle('rotate')
 }
 
 function dropDown2() {
-	document.getElementById("dropdown-2").classList.toggle("show");
+	document.getElementById('dropdown-2').classList.toggle('show')
+	document.getElementById('dropdown-1').classList.remove('show')
+	rotateArrowB.classList.toggle('rotate')
 }
 
-// Close the dropdown if the user clicks outside of it
-// window.onclick = function (event) {
-// 	if (!event.target.matches(".main-nav__link")) {
-// 		var dropdowns = document.getElementsByClassName("main-nav__dropdown");
-// 		var i;
-// 		for (i = 0; i < dropdowns.length; i++) {
-// 			var openDropdown = dropdowns[i];
-// 			if (openDropdown.classList.contains("show")) {
-// 				openDropdown.classList.remove("show");
-// 			}
-// 		}
-// 	}
-// };
+window.addEventListener('click', function (e) {
+	if (
+		!sideNav.contains(e.target) &&
+		!closeBtn.contains(e.target) &&
+		!menuBtn.contains(e.target) &&
+		!dropdownMenu.contains(e.target)
+	) {
+		sideNav.classList.remove('open')
+		bodyOverlay.classList.remove('enabled')
+		document.getElementById('dropdown-2').classList.remove('show')
+		document.getElementById('dropdown-1').classList.remove('show')
+		rotateArrowA.classList.remove('rotate')
+		rotateArrowB.classList.remove('rotate')
+	}
+})
